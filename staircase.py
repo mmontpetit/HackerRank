89b4d59cf1453bbb206aeb712336603332686d52
Micheal Montpetit <micheal.montpetit@qlik.com>
2019-11-15 15:16:15 -0500

    Solutions

---
+++ staircase.py
@@ -0,0 +1,8 @@
+def staircase(n):
+    # The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of n=6.
+    symbols=0
+    image="#"
+    for i in range(n):
+        symbols=i+1
+        spaces=symbols*image
+        print( spaces.rjust(n))
